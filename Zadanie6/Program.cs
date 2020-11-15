ca6cd63dc0abab84cb7ac8e13a69550690e20454
sebnorth <sebnorth@gmail.com>
2020-11-15 13:54:10 +0100

    Dodałem zadanie 6

--- Zadanie6/Program.cs
+++ Zadanie6/Program.cs
@@ -10,6 +10,41 @@ namespace Zadanie6
     {
         static void Main(string[] args)
         {
+            
+            poprawa:
+            
+            try
+            {
+                Console.WriteLine("Podaj x: ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                if (y == 0)
+                {
+                    throw new DivideByZeroException("Proszę podać wartość y różną od zera!!!");
+                }
+
+                int iloraz = x / y;
+
+                Console.WriteLine("iloraz x przez y wynosi: {0}", iloraz);
+            }
+            catch (DivideByZeroException uchwyt)
+            {
+
+                Console.WriteLine(uchwyt.Message);
+                goto poprawa;
+                // throw;
+
+            }
+            catch (Exception uchwyt) {
+
+                Console.WriteLine(uchwyt.Message);
+            }
+
+            Console.WriteLine("Program rusza dalej...");
+            Console.ReadKey();
         }
     }
 }
